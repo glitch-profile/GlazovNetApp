48828e56af53c69d52922bd1b809c7af49e4312a
Ivan <korablev2002@yandex.ru>
2024-01-11 17:31:11 +0300

    added login screen

--- app/src/main/java/com/glazovnet/glazovnetapp/presentation/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/glazovnet/glazovnetapp/presentation/loginscreen/LoginScreen.kt
@@ -1,2 +1,277 @@
 package com.glazovnet.glazovnetapp.presentation.loginscreen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Size
+import com.glazovnet.glazovnetapp.R
+import com.glazovnet.glazovnetapp.presentation.components.DesignedCheckBox
+import com.glazovnet.glazovnetapp.presentation.components.DesignedTextField
+
+@Composable
+fun LoginScreen(
+    navController: NavController,
+    viewModel: LoginViewModel = hiltViewModel()
+) {
+    val loginState = viewModel.loginState.collectAsState().value
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val imageUrl = viewModel.introImageUrl.collectAsState().value
+        if (imageUrl.isNotEmpty()) {
+            println(imageUrl)
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize(),
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Crop,
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = null
+            )
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 8.dp)
+    ) {
+        val cardsBackgroundColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f)
+        val errorCardBackgroundColor = MaterialTheme.colorScheme.errorContainer.copy(alpha = 0.8f)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .background(
+                    color = cardsBackgroundColor,
+                    shape = MaterialTheme.shapes.medium
+                )
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_glazov_net_logo),
+                contentDescription = null)
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .background(
+                    color = cardsBackgroundColor,
+                    shape = MaterialTheme.shapes.medium
+                )
+        ) {
+            AuthForm(
+                viewModel = viewModel,
+                navController = navController
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .background(
+                    color = cardsBackgroundColor,
+                    shape = MaterialTheme.shapes.medium
+                )
+        ) {
+            ActionButtonsForm(
+                isButtonsEnabled = !loginState.isLoading,
+                viewModel = viewModel,
+                navController = navController
+            )
+        }
+        AnimatedVisibility(visible = ( loginState.message != null || loginState.stringResourceId != null )) {
+            Column(
+                Modifier.fillMaxWidth()
+            ) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp)
+                        .background(
+                            color = errorCardBackgroundColor,
+                            shape = MaterialTheme.shapes.medium
+                        )
+                ) {
+                    ErrorMessageScreen(
+                        stringResourceMessage = loginState.stringResourceId,
+                        message = loginState.message
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+private fun AuthForm(
+    navController: NavController,
+    viewModel: LoginViewModel
+) {
+    val username = viewModel.username.collectAsState().value
+    val password = viewModel.password.collectAsState().value
+    val isSaveAuthData = viewModel.saveAuthData.collectAsState().value
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        DesignedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            text = username,
+            onTextEdit = { viewModel.editUsername(it) },
+            placeholder = stringResource(id = R.string.login_screen_username_text),
+            keyboardOptions = KeyboardOptions(autoCorrect = false, imeAction = ImeAction.Next)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        DesignedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            text = password,
+            onTextEdit = { viewModel.editPassword(it) },
+            placeholder = stringResource(id = R.string.login_screen_password_text),
+            keyboardActions = KeyboardActions(
+                onDone = { 
+                    viewModel.login(
+                        isAsAdmin = false,
+                        onLoginSuccessfully = {
+                            TODO()
+                        }
+                    ) 
+                }
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            )
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        DesignedCheckBox(
+            isChecked = isSaveAuthData,
+            onStateChanges = { viewModel.editIsSaveAuthData(it) },
+            label = stringResource(id = R.string.remember_user_text)
+        )
+    }
+}
+
+@Composable
+private fun ActionButtonsForm(
+    isButtonsEnabled: Boolean,
+    navController: NavController,
+    viewModel: LoginViewModel
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        TextButton(
+            onClick = {
+                viewModel.login(
+                    isAsAdmin = true,
+                    onLoginSuccessfully = {
+                        TODO()
+                    }
+                )
+            },
+            enabled = isButtonsEnabled
+        ) {
+            Text(text = stringResource(id = R.string.login_screen_login_as_admin_button))
+        }
+        Button(
+            onClick = { 
+                viewModel.login(
+                    isAsAdmin = false,
+                    onLoginSuccessfully = {
+                        TODO()
+                    }
+                ) 
+            },
+            enabled = isButtonsEnabled
+        ) {
+            Text(text = stringResource(id = R.string.login_screen_login_as_user_button))
+        }
+    }
+}
+
+@Composable
+private fun ErrorMessageScreen(
+    stringResourceMessage: Int?,
+    message: String?
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .animateContentSize()
+    ) {
+        if (stringResourceMessage != null) {
+            Text(
+                text = stringResource(id = stringResourceMessage),
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onErrorContainer
+            )
+        }
+        if (message != null) {
+            Text(
+                text = message,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onErrorContainer,
+                maxLines = 2
+            )
+        }
+    }
+}
--- app/src/main/java/com/glazovnet/glazovnetapp/presentation/loginscreen/LoginViewModel.kt
+++ app/src/main/java/com/glazovnet/glazovnetapp/presentation/loginscreen/LoginViewModel.kt
@@ -1,4 +1,110 @@
 package com.glazovnet.glazovnetapp.presentation.loginscreen
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.glazovnet.glazovnetapp.R
+import com.glazovnet.glazovnetapp.domain.repository.LocalUserAuthDataRepository
+import com.glazovnet.glazovnetapp.domain.repository.UtilsApiRepository
+import com.glazovnet.glazovnetapp.domain.usecase.AuthUseCase
+import com.glazovnet.glazovnetapp.domain.utils.Resource
+import com.glazovnet.glazovnetapp.presentation.ScreenState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val authUseCase: AuthUseCase,
+    private val utilsApiRepository: UtilsApiRepository,
+    private val localUserAuthDataRepository: LocalUserAuthDataRepository
+): ViewModel() {
+
+    private val _introImageUrl = MutableStateFlow("")
+    val introImageUrl = _introImageUrl.asStateFlow()
+
+    private val _username = MutableStateFlow("")
+    val username = _username.asStateFlow()
+    private val _password = MutableStateFlow("")
+    val password = _password.asStateFlow()
+    private val _saveAuthData = MutableStateFlow(false)
+    val saveAuthData = _saveAuthData.asStateFlow()
+
+    private val _loginState = MutableStateFlow(ScreenState<Unit>())
+    val loginState = _loginState.asStateFlow()
+
+    init {
+        loadIntroImage()
+        loadAuthSettings()
+    }
+
+    fun editUsername(username: String) {
+        _username.update { username }
+    }
+    fun editPassword(password: String) {
+        _password.update { password }
+    }
+    fun editIsSaveAuthData(isNeedToSave: Boolean) {
+        _saveAuthData.update { isNeedToSave }
+    }
+
+    private fun loadIntroImage() {
+        viewModelScope.launch {
+            _introImageUrl.update { utilsApiRepository.getIntroImageUrl() }
+        }
+    }
+
+    private fun loadAuthSettings() {
+        with(localUserAuthDataRepository) {
+            _username.update { getSavedUserLogin() ?: "" }
+        }
+    }
+
+    fun login(isAsAdmin: Boolean, onLoginSuccessfully: () -> Unit) {
+        viewModelScope.launch {
+            if (username.value.isNotBlank() && password.value.isNotBlank()) {
+                _loginState.update {
+                    it.copy(
+                        isLoading = true,
+                        message = null,
+                        stringResourceId = null
+                    )
+                }
+                val result = authUseCase.login(
+                    login = username.value,
+                    password = password.value,
+                    asAdmin = isAsAdmin,
+                    isRememberAuthData = saveAuthData.value
+                )
+                when (result) {
+                    is Resource.Success -> {
+                        _loginState.update {
+                            it.copy(
+                                isLoading = false
+                            )
+                        }
+                        onLoginSuccessfully.invoke()
+                    }
+                    is Resource.Error -> {
+                        _loginState.update {
+                            it.copy(
+                                isLoading = false,
+                                stringResourceId = result.stringResourceId,
+                                message = result.message
+                            )
+                        }
+                    }
+                }
+            } else {
+                _loginState.update {
+                    it.copy(
+                        stringResourceId = R.string.login_screen_fields_are_empty_error
+                    )
+                }
+            }
+        }
+    }
 }
