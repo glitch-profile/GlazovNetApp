f2419e832c35256985dfa82138bb5b5f0ae8ea6d
Ivan <korablev2002@yandex.ru>
2024-01-22 15:05:01 +0400

    changed system bars color to transparent. Added windowInsets setting

--- app/src/main/java/com/glazovnet/glazovnetapp/presentation/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/glazovnet/glazovnetapp/presentation/loginscreen/LoginScreen.kt
@@ -18,10 +18,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -100,6 +103,7 @@ fun LoginScreen(
         modifier = Modifier
             .fillMaxSize()
             .padding(vertical = SIDE_PADDING.dp)
+            .windowInsetsPadding(WindowInsets.safeDrawing)
     ) {
         val cardsBackgroundColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f)
         val errorCardBackgroundColor = MaterialTheme.colorScheme.errorContainer.copy(alpha = 0.8f)
--- app/src/main/java/com/glazovnet/glazovnetapp/presentation/mainactivity/MainActivity.kt
+++ app/src/main/java/com/glazovnet/glazovnetapp/presentation/mainactivity/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -22,6 +23,7 @@ class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             GlazovNetAppTheme {
                 // A surface container using the 'background' color from the theme
