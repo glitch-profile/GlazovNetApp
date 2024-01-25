2212fe821bd28df57df35c3e66f0d250f8c797ab
Ivan <korablev2002@yandex.ru>
2024-01-25 17:54:51 +0400

    added chat bubble composable

---
+++ app/src/main/java/com/glazovnet/glazovnetapp/presentation/supportscreen/chat/ChatBubble.kt
@@ -0,0 +1,99 @@
+package com.glazovnet.glazovnetapp.presentation.supportscreen.chat
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.glazovnet.glazovnetapp.R
+import com.glazovnet.glazovnetapp.domain.models.supportrequest.MessageModel
+import com.glazovnet.glazovnetapp.domain.utils.getLocalizedOffsetString
+import java.time.OffsetDateTime
+
+@Preview
+@Composable
+fun ChatBubblePreview() {
+    ChatBubble(
+        senderName = "Кораблев Иван",
+        text = "Привет! Пойдешь в диз?",
+        timestamp = OffsetDateTime.now(),
+        isOwnMessage = true,
+        isNeedToShowAuthor = true
+    )
+}
+
+@Composable
+fun ChatBubble(
+    modifier: Modifier = Modifier,
+    senderName: String,
+    text: String,
+    timestamp: OffsetDateTime,
+    isOwnMessage: Boolean,
+    isNeedToShowAuthor: Boolean,
+    maxBubbleWidth: Dp = 250.dp,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = if (isOwnMessage) Arrangement.End else Arrangement.Start
+    ) {
+        val backgroundColor = if (isOwnMessage) MaterialTheme.colorScheme.primary
+        else MaterialTheme.colorScheme.primaryContainer
+        val textColor = if (isOwnMessage) MaterialTheme.colorScheme.onPrimary
+        else MaterialTheme.colorScheme.onPrimaryContainer
+        val bubbleShape = if (isOwnMessage) RoundedCornerShape(
+            topStart = 12.dp, topEnd = 4.dp, bottomEnd = 12.dp, bottomStart = 12.dp
+        ) else RoundedCornerShape(
+            topStart = 4.dp, topEnd = 12.dp, bottomEnd = 12.dp, bottomStart = 12.dp
+        )
+        Column {
+            Text(
+                modifier = Modifier
+                    //.padding(horizontal = 8.dp)
+                    .align(Alignment.End),
+                text = if (!isOwnMessage) senderName
+                else stringResource(id = R.string.chat_bubble_screen_sender_name_self),
+                style = MaterialTheme.typography.titleSmall,
+                color = textColor
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Column(
+                modifier = Modifier
+                    .clip(bubbleShape)
+                    .background(backgroundColor)
+                    .padding(8.dp)
+                    .widthIn(max = maxBubbleWidth)
+            ) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = textColor
+                )
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.End),
+                    text = timestamp.getLocalizedOffsetString(
+                        daysPattern = "dd MMMM"
+                    ),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = textColor
+                )
+            }
+
+        }
+    }
+}
