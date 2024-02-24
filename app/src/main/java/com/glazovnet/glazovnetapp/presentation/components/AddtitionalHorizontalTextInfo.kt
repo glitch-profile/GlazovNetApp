222515be4a9d343aea59bce08e71dffefcde5bb0
Ivan <korablev2002@yandex.ru>
2024-02-24 16:30:32 +0400

    moved to standalone component

---
+++ app/src/main/java/com/glazovnet/glazovnetapp/presentation/components/AddtitionalHorizontalTextInfo.kt
@@ -0,0 +1,43 @@
+package com.glazovnet.glazovnetapp.presentation.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AdditionalTextInfo(
+    modifier: Modifier = Modifier,
+    title: String,
+    text: String
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier,
+            text = title,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f),
+            text = text,
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
