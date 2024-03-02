8208fa80250aa64c6cdd1a6fe2437573400b59a0
Ivan <korablev2002@yandex.ru>
2024-03-02 18:40:18 +0500

    updated app version to 0.7.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.5.1"
+        versionName = "0.7.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
