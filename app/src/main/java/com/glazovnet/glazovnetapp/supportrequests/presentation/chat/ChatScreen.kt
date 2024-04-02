1115771d0f5d5514ed8b8f82949e88d6373443d3
Ivan <korablev2002@yandex.ru>
2024-04-02 20:05:23 +0500

    added JumpToBottomButton for chats

--- app/src/main/java/com/glazovnet/glazovnetapp/supportrequests/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/glazovnet/glazovnetapp/supportrequests/presentation/chat/ChatScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -42,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -58,6 +60,8 @@ import com.glazovnet.glazovnetapp.supportrequests.domain.model.MessageModel
 import com.glazovnet.glazovnetapp.supportrequests.domain.model.RequestStatus
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+
+private val maxScrollOffsetToJump = 112.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -177,42 +181,64 @@ private fun MessagesList(
             )
         }
     } else {
-        LazyColumn(
-            modifier = modifier,
-            reverseLayout = true,
-            state = lazyListState
+        Box(
+            modifier = modifier
         ) {
-            for (index in messages.indices) {
-                val previousSenderId = messages.getOrNull(index + 1)?.senderId
-                val isSameSender = previousSenderId == messages[index].senderId
-                val topPadding = if (isSameSender) 0.dp else 4.dp
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                reverseLayout = true,
+                state = lazyListState
+            ) {
+                for (index in messages.indices) {
+                    val previousSenderId = messages.getOrNull(index + 1)?.senderId
+                    val isSameSender = previousSenderId == messages[index].senderId
+                    val topPadding = if (isSameSender) 0.dp else 4.dp
 
-                item(
-                    key = messages[index].id
-                ) {
-                    with(messages[index]) {
-                        ChatBubble(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(
-                                    start = 16.dp,
-                                    end = 16.dp,
-                                    top = topPadding,
-                                    bottom = 4.dp
-                                )
-                                .animateItemPlacement(
-                                    animationSpec = tween(durationMillis = 300)
-                                ),
-                            senderName = this.senderName,
-                            text = this.text,
-                            timestamp = this.timestamp!!,
-                            isOwnMessage = this.isOwnMessage,
-                            isSameSender = isSameSender,
-                            maxBubbleWidth = 280.dp
-                        )
+                    item(
+                        key = messages[index].id
+                    ) {
+                        with(messages[index]) {
+                            ChatBubble(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(
+                                        start = 16.dp,
+                                        end = 16.dp,
+                                        top = topPadding,
+                                        bottom = 4.dp
+                                    )
+                                    .animateItemPlacement(
+                                        animationSpec = tween(durationMillis = 300)
+                                    ),
+                                senderName = this.senderName,
+                                text = this.text,
+                                timestamp = this.timestamp!!,
+                                isOwnMessage = this.isOwnMessage,
+                                isSameSender = isSameSender,
+                                maxBubbleWidth = 280.dp
+                            )
+                        }
                     }
                 }
             }
+            val jumpThreshold = with(LocalDensity.current) {
+                maxScrollOffsetToJump.toPx()
+            }
+            val isJumpToBottomVisible = derivedStateOf {
+                lazyListState.firstVisibleItemIndex != 0
+                        || lazyListState.firstVisibleItemScrollOffset > jumpThreshold
+            }
+            JumpToBottomButton(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter),
+                enabled = isJumpToBottomVisible.value,
+                onClicked = {
+                    coroutineScope.launch {
+                        lazyListState.animateScrollToItem(0)
+                    }
+                }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/glazovnet/glazovnetapp/supportrequests/presentation/chat/JumpToBottomButton.kt
@@ -0,0 +1,77 @@
+package com.glazovnet.glazovnetapp.supportrequests.presentation.chat
+
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.glazovnet.glazovnetapp.R
+
+private enum class Visibility {
+    VISIBLE, GONE
+}
+
+@Composable
+fun JumpToBottomButton(
+    modifier: Modifier = Modifier,
+    enabled: Boolean,
+    onClicked: () -> Unit
+) {
+    val transition = updateTransition(
+        if (enabled) Visibility.VISIBLE else Visibility.GONE,
+        label = "JumpToBottomButton State changes"
+    )
+    val bottomOffset = transition.animateDp(label = "JumpToBottomButton offset transition") {
+        if (it == Visibility.GONE) {
+            (-48).dp
+        } else {
+            16.dp
+        }
+    }
+    if (bottomOffset.value != (-48).dp) {
+        val surfaceColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+        Box(
+            modifier = modifier
+                .offset(x = 0.dp, y = -bottomOffset.value)
+        ) {
+            Row(
+                modifier = modifier
+                    .clip(MaterialTheme.shapes.medium)
+                    .clickable { onClicked.invoke() }
+                    .background(surfaceColor)
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = stringResource(id = R.string.request_chat_jump_to_bottom_text),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
+    }
+
+
+}
