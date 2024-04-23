0d80dc0ff7f7a3fb5ea0abeccf62b20049007208
Ivan <korablev2002@yandex.ru>
2024-04-24 00:39:53 +0400

    added LoadingComponent. Marked LoadingIndicator as deprecated. Should delete later

--- app/src/main/java/com/glazovnet/glazovnetapp/core/presentation/components/LoadingIndicator.kt
+++ app/src/main/java/com/glazovnet/glazovnetapp/core/presentation/components/LoadingIndicator.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.glazovnet.glazovnetapp.R
 
+@Deprecated("Use loading component instead", level = DeprecationLevel.WARNING)
 @Composable
 fun LoadingIndicator(
     modifier: Modifier = Modifier,
