5e4017e8cd0245e0b67b8f2b2016cf57548bfc05
Ivan <korablev2002@yandex.ru>
2024-05-06 01:21:44 +0400

    added paddings after sectors with buttons

--- app/src/main/java/com/glazovnet/glazovnetapp/settings/appearance/presentation/AppearanceSettingsScreen.kt
+++ app/src/main/java/com/glazovnet/glazovnetapp/settings/appearance/presentation/AppearanceSettingsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -199,6 +200,8 @@ fun AppearanceSettingsScreen(
                     }
                 )
             }
+            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.navigationBarsPadding())
         }
     }
 }
